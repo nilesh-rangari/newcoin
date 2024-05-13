30f63850edcf6070022b820458ecadf64d27e85a
Nilesh <nvrangari@gmail.com>
2024-05-13 12:00:47 +0530

    upcoming projects section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -590,6 +590,134 @@ export default function Home(){
                     </div>
                 </section>
 
+                {/* section 3 E  */}
+
+                <section className="min-h-[414px] md:min-h-[768px] relative" id="section_upcoming">
+                    <div className="w-full px-8 sm:px-12 md:px-16 max-w-7xl mx-auto">
+                    <div className="flex items-center justify-center flex-col text-center py-10 pt-24 relative z-10">
+                        <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-5 text-center">
+                            Upcoming <span className="text-[#00A3FF]">Contracts</span> 
+                            <br/>
+                            Integration
+                        </h2>
+                        <p className="text-sm md:w-[46ch]">On Global Pool ROI</p>
+                        <div className="grid lg:grid-cols-2 gap-10 pt-20">
+                        <div 
+                            // style="opacity: 0; transform: scale(0.85) translateZ(0px);"
+                            >
+                            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                            <div className="flex items-start gap-5 sm:gap-8">
+                                <div 
+                                    style={{backgroundImage: "linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)"}}
+                                    // style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" 
+                                    className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" 
+                                    aria-hidden="true"
+                                >
+                                    01
+                                </div>
+                            <div>
+                                <h3 className="text-lg font-medium font-sora sm:text-xl sm:font-semibold">
+                                    Discount Crypto
+                                </h3>
+                                <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">
+                                    Offering Discounted Crypto for CIP Pro Tokens
+                                </p>
+                                </div>
+                            </div>
+                            </article>
+                        </div>
+                        <div 
+                            // style="opacity: 0; transform: scale(0.85) translateZ(0px);"
+                            >
+                            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                            <div className="flex items-start gap-5 sm:gap-8">
+                                <div 
+                                    style={{backgroundImage: "linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)"}}
+                                    // style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" 
+                                    className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" 
+                                    aria-hidden="true"
+                                    >
+                                        02
+                                </div>
+                                <div>
+                                <h3 className="text-lg font-medium font-sora sm:text-xl sm:font-semibold">
+                                    Multichain Bridge
+                                </h3>
+                                <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">One click solution to bridge Multichain Token</p>
+                                </div>
+                            </div>
+                            </article>
+                        </div>
+                        <div 
+                            // style="opacity: 0; transform: scale(0.85) translateZ(0px);"
+                            >
+                            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                            <div className="flex items-start gap-5 sm:gap-8">
+                                <div 
+                                    style={{backgroundImage: "linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)"}}
+                                    // style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" 
+                                    className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" 
+                                    aria-hidden="true"
+                                    >
+                                        03
+                                </div>
+                                <div>
+                                <h3 className="text-lg font-medium font-sora sm:text-xl sm:font-semibold">
+                                    Rule of Seven
+                                </h3>
+                                <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">
+                                    Compounding Profit and Capital followed by Seven Rules of Buying Assets to support Liquidity.
+                                </p>
+                                </div>
+                            </div>
+                            </article>
+                        </div>
+                        <div 
+                            // style="opacity: 0; transform: scale(0.85) translateZ(0px);"
+                            >
+                            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                            <div className="flex items-start gap-5 sm:gap-8">
+                                <div 
+                                    style={{backgroundImage: 'linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)'}}
+                                    // style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" 
+                                    className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" 
+                                    aria-hidden="true">
+                                        04
+                                </div>
+                                <div>
+                                <h3 className="text-lg font-medium font-sora sm:text-xl sm:font-semibold">
+                                    NFTS
+                                </h3>
+                                <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">
+                                    Limited Edition of Non-Fungible Tokens benefitting the community.
+                                </p>
+                                </div>
+                            </div>
+                            </article>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    <div className="absolute -left-[350px] -top-[100px]">
+                    <div className="w-[800px] h-[800px] relative z-0">
+                        <img 
+                            alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" 
+                            className='absolute h-full w-full left-0 top-0 right-0 bottom-0 object-contain text-transparent'
+                            src={Shadow}
+                        />
+                    </div>
+                    </div>
+                    <div className="absolute -right-[350px] -bottom-[300px]">
+                    <div className="w-[800px] h-[800px] relative z-0">
+                        <img 
+                            alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" 
+                            className='absolute h-full w-full left-0 top-0 right-0 bottom-0 object-contain text-transparent'
+                            src={Shadow}
+                        />
+                    </div>
+                    </div>
+                </section>
+
             </div>
         </div>
     )
--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -203,80 +203,80 @@
             </section>
 
   5 four 
-  <section className="min-h-[414px] md:min-h-[768px] relative" id="section_upcoming">
-    <div className="w-full px-8 sm:px-12 md:px-16 max-w-7xl mx-auto">
-      <div className="flex items-center justify-center flex-col text-center py-10 pt-24 relative z-10">
-        <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-5 text-center">Upcoming <span className="text-[#00A3FF]">Contracts</span>
-          <br>Integration
-        </h2>
-        <p className="text-sm md:w-[46ch]">On Global Pool ROI</p>
-        <div className="grid lg:grid-cols-2 gap-10 pt-20">
-          <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
-            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
-              <div className="flex items-start gap-5 sm:gap-8">
-                <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
-                  <!-- -->1
-                </div>
-                <div>
-                  <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Discount Crypto</h3>
-                  <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Offering Discounted Crypto for CIP Pro Tokens</p>
+            <section className="min-h-[414px] md:min-h-[768px] relative" id="section_upcoming">
+              <div className="w-full px-8 sm:px-12 md:px-16 max-w-7xl mx-auto">
+                <div className="flex items-center justify-center flex-col text-center py-10 pt-24 relative z-10">
+                  <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold mb-5 text-center">Upcoming <span className="text-[#00A3FF]">Contracts</span>
+                    <br>Integration
+                  </h2>
+                  <p className="text-sm md:w-[46ch]">On Global Pool ROI</p>
+                  <div className="grid lg:grid-cols-2 gap-10 pt-20">
+                    <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
+                      <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                        <div className="flex items-start gap-5 sm:gap-8">
+                          <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
+                            <!-- -->1
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Discount Crypto</h3>
+                            <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Offering Discounted Crypto for CIP Pro Tokens</p>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                    <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
+                      <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                        <div className="flex items-start gap-5 sm:gap-8">
+                          <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
+                            <!-- -->2
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Multichain Bridge</h3>
+                            <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">One click solution to bridge Multichain Token</p>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                    <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
+                      <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                        <div className="flex items-start gap-5 sm:gap-8">
+                          <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
+                            <!-- -->3
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Rule of Seven</h3>
+                            <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Compounding Profit and Capital followed by Seven Rules of Buying Assets to support Liquidity.</p>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                    <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
+                      <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
+                        <div className="flex items-start gap-5 sm:gap-8">
+                          <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
+                            <!-- -->4
+                          </div>
+                          <div>
+                            <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">NFTS</h3>
+                            <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Limited Edition of Non-Fungible Tokens benefitting the community.</p>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </article>
-          </div>
-          <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
-            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
-              <div className="flex items-start gap-5 sm:gap-8">
-                <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
-                  <!-- -->2
-                </div>
-                <div>
-                  <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Multichain Bridge</h3>
-                  <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">One click solution to bridge Multichain Token</p>
+              <div className="absolute -left-[350px] -top-[100px]">
+                <div className="w-[800px] h-[800px] relative z-0">
+                  <img alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;color:transparent" src="/images/shadow.svg">
                 </div>
               </div>
-            </article>
-          </div>
-          <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
-            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
-              <div className="flex items-start gap-5 sm:gap-8">
-                <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
-                  <!-- -->3
-                </div>
-                <div>
-                  <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">Rule of Seven</h3>
-                  <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Compounding Profit and Capital followed by Seven Rules of Buying Assets to support Liquidity.</p>
+              <div className="absolute -right-[350px] -bottom-[300px]">
+                <div className="w-[800px] h-[800px] relative z-0">
+                  <img alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;color:transparent" src="/images/shadow.svg">
                 </div>
               </div>
-            </article>
-          </div>
-          <div style="opacity: 0; transform: scale(0.85) translateZ(0px);">
-            <article className="text-start rounded-lg bg-[#6A6A6A1A] p-4 sm:p-6 lg:p-12 shadow-2xl">
-              <div className="flex items-start gap-5 sm:gap-8">
-                <div style="background:linear-gradient(115.23deg, #109BFF 16.25%, #1F1C29 230.73%)" className="aspect-square rounded-lg text-xl lg:text-2xl font-semibold grid h-12 lg:h-14 place-content-center" aria-hidden="true">0
-                  <!-- -->4
-                </div>
-                <div>
-                  <h3 className="text-lg font-medium font-sora sm:text-xl font-semibold">NFTS</h3>
-                  <p className="text-sm text-neutral-400 mt-1 sm:mt-2 lg:mt-3">Limited Edition of Non-Fungible Tokens benefitting the community.</p>
-                </div>
-              </div>
-            </article>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="absolute -left-[350px] -top-[100px]">
-      <div className="w-[800px] h-[800px] relative z-0">
-        <img alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;color:transparent" src="/images/shadow.svg">
-      </div>
-    </div>
-    <div className="absolute -right-[350px] -bottom-[300px]">
-      <div className="w-[800px] h-[800px] relative z-0">
-        <img alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;color:transparent" src="/images/shadow.svg">
-      </div>
-    </div>
-  </section>
+            </section>
 
   6 six 
   <section className="min-h-[414px] md:min-h-[768px] relative">
