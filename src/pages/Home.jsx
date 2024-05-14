716fe8334354b5871a12be5ce4895d58bea4850a
Nilesh <nvrangari@gmail.com>
2024-05-14 22:47:10 +0530

    footer section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,8 @@ import diamond from '../assets/diamond.svg'
 import silverStar from '../assets/silver-star.svg'
 import thinStar from '../assets/thin-star.svg'
 import arrowDown from '../assets/arrow-down.svg'
-
+import phone from '../assets/phone.svg'
+import mail from '../assets/mail.svg'
 
 
 
@@ -1163,6 +1164,117 @@ export default function Home(){
                     </div>
                     </div>
                 </section>
+
+                {/* footer sectioon  */}
+
+                <div className="relative">
+                    <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+                    <div className="grid row-gap-10 mb-8 lg:grid-cols-6">
+                        <div className="grid grid-cols-2 gap-8 lg:col-span-4 md:grid-cols-4">
+                        <div className="flex flex-col gap-4">
+                            <div className="relative h-10 aspect-square mb-3">
+                            <img 
+                                alt="Logo" loading="lazy" decoding="async" data-nimg="fill" 
+                                className="object-contain object-left absolute h-full w-full left-0 top-0 right-0 bottom-0 text-transparent" 
+                                // style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;color:transparent" 
+                                src={Logo}
+                            />
+                            </div>
+                            <a className="text-neutral-300 text-sm flex items-center gap-2" href="/">
+                                <img 
+                                    alt="Mail Icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" 
+                                    // style="color:transparent" 
+                                    className='text-transparent'
+                                    src={mail}
+                                />
+                                    help@frybix.com 
+                            </a>
+                            <a className="text-neutral-300 text-sm flex items-center gap-2" href="/">
+                                <img 
+                                    alt="Phone Icon" loading="lazy" width="20" height="20" decoding="async" data-nimg="1" 
+                                    // style="color:transparent" 
+                                    className='text-transparent'
+                                    src={phone}
+                                />
+                                    +1 234 456 678 89 
+                            </a>
+                        </div>
+                        <div>
+                            <p className="font-medium text-xl mb-3 tracking-wide capitalize">Links</p>
+                            <ul className="mt-2 space-y-2">
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">Home</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">About Us</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">Bookings</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">Blog</a>
+                            </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p className="font-medium text-xl mb-3 tracking-wide capitalize">legal</p>
+                            <ul className="mt-2 space-y-2">
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">terms of use</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">privacy policy</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">cookie policy</a>
+                            </li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p className="font-medium text-xl mb-3 tracking-wide capitalize">product</p>
+                            <ul className="mt-2 space-y-2">
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">take tour</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">live chat</a>
+                            </li>
+                            <li>
+                                <a href="/" className="text-neutral-300 text-sm capitalize">reveiws</a>
+                            </li>
+                            </ul>
+                        </div>
+                        </div>
+                        <div className="md:max-w-md lg:col-span-2 row-start-1 lg:row-start-auto mb-10 lg:mb-0">
+                        <span className="font-medium text-xl tracking-wid">Newsletter</span>
+                        <p className="text-neutral-400 text-sm mt-3">Stay Up To Date</p>
+                        <form className="flex mt-4 bg-neutral-800 p-2 rounded-lg max-w-[90%] lg:w-[75%]">
+                            <input 
+                                placeholder="Your Email" required="" type="text" 
+                                className="text-sm flex-grow w-full h-12 px-4 transition duration-200 bg-transparent rounded shadow-sm appearance-none md:mr-2 md:mb-0 focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                            />
+                            <button className="inline-block primary-button">
+                                Subscribe
+                            </button>
+                        </form>
+                        </div>
+                    </div>
+                    <hr className="border-none h-[1px] bg-gradient-to-r from-[#16161600] via-[#69696988] to-[#16161600]" />
+                    <div className="flex justify-center pt-5 pb-10">
+                        <p className="text-sm">Copyright 2022 uifry.com All Rights Reserved</p>
+                    </div>
+                    </div>
+                    <div className="absolute -right-[300px] -bottom-[150px] -z-10">
+                    <div className="w-[800px] h-[800px] relative ">
+                        <img 
+                            alt="Shadow Effect" loading="lazy" decoding="async" data-nimg="fill" 
+                            className='absolute h-full w-full left-0 top-0 right-0 bottom-0 object-contain text-transparent'
+                            // style="position:absolute;height:100%;width:100%;left:0;top:0;right:0;bottom:0;object-fit:contain;color:transparent" 
+                            src={Shadow}
+                        />
+                    </div>
+                    </div>
+                </div>
 
             </div>
         </div>
